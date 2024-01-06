bd0902dfc49ceb24444ba0fead766c88aa954122
whai <lkwhai@163.com>
2024-01-06 15:40:09 +0800

    --

---
+++ src/main/java/cn/whaifree/redo/LeetCode202.java
@@ -0,0 +1,43 @@
+package cn.whaifree.redo;
+
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LeetCode202 {
+
+    @Test
+    public void test() {
+        System.out.println(new Solution().isHappy(2));
+    }
+
+    class Solution {
+        /**
+         * 无限循环
+         * @param n
+         * @return
+         */
+        public boolean isHappy(int n) {
+            Set<Integer> set = new HashSet<>();
+            while (n != 1) {
+                if (set.contains(n)) {
+                    return false;
+                }
+                set.add(n);
+                n = get(n);
+            }
+            return true;
+        }
+
+        public int get(int n) {
+            int sum = 0;
+            while (n != 0) {
+                int e = n % 10;
+                sum += e * e;
+                n /= 10;
+            }
+            return sum;
+        }
+    }
+}
---
+++ src/main/java/cn/whaifree/redo/LeetCode287.java
@@ -0,0 +1,51 @@
+package cn.whaifree.redo;
+
+import cn.hutool.core.lang.hash.Hash;
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LeetCode287 {
+
+    @Test
+    public void test() {
+        System.out.println(new Solution().findDuplicate(new int[]{1, 3, 4, 3, 2}));
+    }
+
+    class Solution {
+        /**
+         * 要求 O（N）的时间复杂度
+         *     O（1）的控空间
+         *
+         *     将数组转换为链表的思想，即求有没有形成环
+         *     1 3 4 2 2
+         *     0 1 2 3 4
+         *  0-->1-->2-->4--->2
+         *
+         * @param nums
+         * @return
+         */
+        public int findDuplicate(int[] nums) {
+            // 1. 找到相遇的点
+            // 2. 将快指针指向首部
+            // 3. 两个指针重合处就是
+
+            int fast = 0;
+            int slow = 0;
+
+            do {
+                fast = nums[nums[fast]];
+                slow = nums[slow];
+            } while (fast != slow);
+
+            fast = 0;
+            while (fast != slow) {
+                fast = nums[fast];
+                slow = nums[slow];
+            }
+
+            return fast;
+        }
+    }
+}
