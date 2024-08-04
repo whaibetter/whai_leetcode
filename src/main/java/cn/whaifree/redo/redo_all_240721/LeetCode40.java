b20f5f042fab0e696fa23efd38d627d8a85c198e
whaifree <49432110+whaibetter@users.noreply.github.com>
2024-08-04 16:37:30 +0800

    update whaifree redo demo更新了whaifree重做演示代码，新增了几个LeetCode题目（46、47、51、491）的解决方案，修改了40题的解法，包括回溯算法的实现和一些测试用例的输出方式。确保了代码的正确性和可读性。

--- src/main/java/cn/whaifree/redo/redo_all_240721/LeetCode40.java
+++ src/main/java/cn/whaifree/redo/redo_all_240721/LeetCode40.java
@@ -11,14 +11,9 @@ import java.util.*;
 
 public class LeetCode40 {
     public static void main(String[] args) {
-        new LeetCode40().combinationSum2(new int[]{14,6,25,9,30,20,33,34,28,30,16,12,31,9,9,12,34,16,25,32,8,7,30,12,33,20,21,29,24,17,27,34,11,17,30,6,32,21,27,17,16,8,24,12,12,28,11,33,10,32,22,13,34,18,12}, 27).forEach(
-                list -> {
-                    list.forEach(
-                            integer -> System.out.print(integer + " ")
-                    );
-                    System.out.println();
-                }
-        );
+        new Solution().combinationSum2(new int[]{10, 1, 2, 7, 6, 1, 5}, 8).forEach(list -> {
+            System.out.println(list);
+        });
     }
 
     List<List<Integer>> res = new ArrayList<>();
@@ -53,4 +48,45 @@ public class LeetCode40 {
             nowSum -= candidates[i];
         }
     }
+
+
+    static class Solution {
+
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+
+        /**
+         *
+         *
+         * @param candidates
+         * @param target
+         * @return
+         */
+        public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+            Arrays.sort(candidates);
+            backTracking(candidates, 0, target);
+            return res;
+        }
+
+        public void backTracking(int[] candidates, int index, int need) {
+            if (need < 0) {
+                return;
+            }
+            if (need == 0) {
+                res.add(new ArrayList<>(path));
+            }
+            HashSet<Integer> set = new HashSet<>();
+            for (int i = index; i < candidates.length; i++) {
+                if (set.contains(candidates[i])) {
+                    continue;
+                }
+                set.add(candidates[i]);
+                path.add(candidates[i]);
+                backTracking(candidates, i + 1, need - candidates[i]);
+                path.remove(path.size() - 1);
+            }
+        }
+
+    }
+
 }
