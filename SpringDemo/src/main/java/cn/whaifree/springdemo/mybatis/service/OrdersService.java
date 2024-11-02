cec43302c886f417fe7e7c6f0ea1c8c823d9bd0d
whaifree <whaifree@163.com>
2024-11-02 13:09:33 +0800

    feat(SpringDemo): 添加订单相关实体类和 Mapper
    - 新增 Orders 实体类，用于订单相关操作
    - 添加 OrdersMapper 接口和对应的 XML 文件，实现订单数据的持久化
    - 创建 OrdersService 接口和 OrdersServiceImpl 实现类，提供订单业务逻辑支持- 在 pom.xml 中添加 packaging标签，指定项目打包方式为 pom

---
+++ SpringDemo/src/main/java/cn/whaifree/springdemo/mybatis/service/OrdersService.java
@@ -0,0 +1,13 @@
+package cn.whaifree.springdemo.mybatis.service;
+
+import cn.whaifree.springdemo.mybatis.domain.Orders;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author wenhai
+* @description 针对表【orders】的数据库操作Service
+* @createDate 2024-11-01 12:45:47
+*/
+public interface OrdersService extends IService<Orders> {
+
+}
