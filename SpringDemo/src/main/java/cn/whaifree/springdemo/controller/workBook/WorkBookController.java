f37f1dd4b305b7c79bc3eee4528015164dbcb564
whai <whaifree@163.com>
2024-11-24 11:05:34 +0800

    refactor(workBook): 重构 WorkBookController 并添加通用导出工具类
    - 将 WorkBookController 重构为 RestController
    - 新增 WorkBookResponseUtils 工具类，用于通用的 Excel导出功能
    - 优化导入导出逻辑，支持任意对象列表的导出
    - 添加单元格值设置异常处理

--- SpringDemo/src/main/java/cn/whaifree/springdemo/controller/workBook/WorkBookController.java
+++ SpringDemo/src/main/java/cn/whaifree/springdemo/controller/workBook/WorkBookController.java
@@ -6,6 +6,7 @@ import com.google.common.collect.Lists;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.Data;
 import lombok.Setter;
+import org.apache.poi.ss.formula.functions.T;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
@@ -17,8 +18,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.*;
+import java.lang.reflect.Field;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
@@ -29,9 +32,13 @@ import java.util.List;
  * @Date 2024/11/20 16:35
  * @注释
  */
-@Controller
+@RestController
 @RequestMapping("/workBook")
 public class WorkBookController {
+
+
+
+
     @GetMapping(value = "/exportUser")
     public void export(HttpServletResponse response) throws UnsupportedEncodingException {
         // WorkBook
