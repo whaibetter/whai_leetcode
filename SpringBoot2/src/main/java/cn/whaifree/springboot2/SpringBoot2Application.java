f5799a04abe5e3c0b961ec79aae42db56cd0336b
whai <whaifree@163.com>
2024-12-04 18:15:25 +0800

    feat: 添加 SpringBoot2 项目基础结构
    - 创建 SpringBoot2 项目并配置 pom.xml
    - 添加应用属性文件和基本代码结构
    - 创建测试用例文件

---
+++ SpringBoot2/src/main/java/cn/whaifree/springboot2/SpringBoot2Application.java
@@ -0,0 +1,14 @@
+package cn.whaifree.springboot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringBoot2Application.class, args);
+    }
+
+}
